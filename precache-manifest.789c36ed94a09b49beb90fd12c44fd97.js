1dd9bb0bc2d5b25f1d19c27579f013cc38841b87
Code Sandbox <hello@codesandbox.io>
2022-04-07 10:49:19 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.789c36ed94a09b49beb90fd12c44fd97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c824c616469fb2ad147648a3690e61e",
+    "url": "https://erience203.github.io/csb-yj3xo9/index.html"
+  },
+  {
+    "revision": "46ceae2166b5e53114a5",
+    "url": "https://erience203.github.io/csb-yj3xo9/static/css/main.1e8d7644.chunk.css"
+  },
+  {
+    "revision": "f76752fb4f31403100dd",
+    "url": "https://erience203.github.io/csb-yj3xo9/static/js/2.2efe7526.chunk.js"
+  },
+  {
+    "revision": "46ceae2166b5e53114a5",
+    "url": "https://erience203.github.io/csb-yj3xo9/static/js/main.0207b7b7.chunk.js"
+  },
+  {
+    "revision": "9c6bf167318bd64325aa",
+    "url": "https://erience203.github.io/csb-yj3xo9/static/js/runtime-main.3f9b0677.js"
+  },
+  {
+    "revision": "2b4d3722a0204432c77e78312a3d24c1",
+    "url": "https://erience203.github.io/csb-yj3xo9/static/media/pu.2b4d3722.glb"
+  }
+]);
